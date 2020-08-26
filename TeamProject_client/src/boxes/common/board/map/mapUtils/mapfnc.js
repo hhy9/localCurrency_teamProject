e3b9482b472f03ffec9a81763dcc6e695136aa74
황혜영 <62981058+hhy9@users.noreply.github.com>
2020-08-26 09:40:02 +0900

    final commit

---
+++ TeamProject_client/src/boxes/common/board/map/mapUtils/mapfnc.js
@@ -0,0 +1,16 @@
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyCzhclpVLv4R5FOEjmP6386O4QItwCYdHA");
+
+export const getLatLng = (location) =>{
+    Geocode.fromAddress(location).then(
+        response => {
+            const resLatLng = response.results[0].geometry.location;
+            return {lat:resLatLng.lat, lng:resLatLng.lng};
+        },
+        error => {
+            console.error(error);
+        }
+    );
+}
+
